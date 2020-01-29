eb90377d52421a31e09e681bdd9aeafcc454515f
dani <dani.zaghian@gmail.com>
2020-01-29 14:03:58 -0800

    adds decomposition

--- whiteboarding2.rb
+++ whiteboarding2.rb
@@ -6,3 +6,15 @@
 # write a function that prints out each letter of the first string one line at a time
 # write a function that loops through each letter of the first string, and compares each letter to each letter of the second string (print "match" if the letters match)
 
+
+
+# Write a function that takes in an array of numbers and returns a new array of the same numbers without any duplicates.
+# Example input: [3, 7, 2, 7, 9, 12, 2]
+# Example output: [3, 7, 2, 9, 12]
+
+# write a function that prints out each number in the array
+# write a function that loops through each number in the array and compare it to all other numbers in the array
+
+
+
+
