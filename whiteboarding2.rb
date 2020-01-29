bf202dcce1a3fca064270c920246a09c7762cc0c
dani <dani.zaghian@gmail.com>
2020-01-29 13:36:38 -0800

    adds decomposed problem

---
+++ whiteboarding2.rb
@@ -0,0 +1,8 @@
+# Example whiteboarding problem: Write a function that takes in two strings and returns the longest common substring.
+# Example input: "together", "bothersome"
+# Example output: "ther"
+
+# write a function that prints out each string
+# write a function that prints out each letter of the first string one line at a time
+# write a function that loops through each letter of the first string, and compares each letter to each letter of the second string (print "match" if the letters match)
+
